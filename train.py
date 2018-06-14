b7f09abe030dbba9d657fb6878be55e442df7c63
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-06-14 15:46:43 +0200

    updates

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -9,76 +10,101 @@ from utils.parse_config import *
 from utils.utils import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=30, help='number of epochs')
-parser.add_argument('--image_folder', type=str, default='data/samples', help='path to dataset')
-parser.add_argument('--batch_size', type=int, default=16, help='size of each image batch')
-parser.add_argument('--model_config_path', type=str, default='config/yolov3.cfg', help='path to model config file')
-parser.add_argument('--data_config_path', type=str, default='config/coco.data', help='path to data config file')
-parser.add_argument('--weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
-parser.add_argument('--class_path', type=str, default='data/coco.names', help='path to class label file')
-parser.add_argument('--conf_thres', type=float, default=0.8, help='object confidence threshold')
-parser.add_argument('--nms_thres', type=float, default=0.4, help='iou thresshold for non-maximum suppression')
-parser.add_argument('--n_cpu', type=int, default=0, help='number of cpu threads to use during batch generation')
-parser.add_argument('--img_size', type=int, default=416, help='size of each image dimension')
-parser.add_argument('--checkpoint_interval', type=int, default=1, help='interval between saving model weights')
-parser.add_argument('--checkpoint_dir', type=str, default='checkpoints', help='directory for saving model checkpoints')
+parser.add_argument('-epochs', type=int, default=30, help='number of epochs')
+parser.add_argument('-image_folder', type=str, default='data/samples', help='path to dataset')
+parser.add_argument('-batch_size', type=int, default=4, help='size of each image batch')
+parser.add_argument('-model_config_path', type=str, default='config/yolovx.cfg', help='path to model config file')
+parser.add_argument('-data_config_path', type=str, default='config/xview.data', help='path to data config file')
+parser.add_argument('-weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
+parser.add_argument('-class_path', type=str, default='data/xview.names', help='path to class label file')
+parser.add_argument('-conf_thres', type=float, default=0.8, help='object confidence threshold')
+parser.add_argument('-nms_thres', type=float, default=0.4, help='iou thresshold for non-maximum suppression')
+parser.add_argument('-n_cpu', type=int, default=2, help='number of cpu threads to use during batch generation')
+parser.add_argument('-img_size', type=int, default=32 * 27, help='size of each image dimension')
+parser.add_argument('-checkpoint_interval', type=int, default=1, help='interval between saving model weights')
+parser.add_argument('-checkpoint_dir', type=str, default='checkpoints', help='directory for saving model checkpoints')
 opt = parser.parse_args()
 print(opt)
 
-os.makedirs('output', exist_ok=True)
-os.makedirs('checkpoints', exist_ok=True)
 
-cuda = torch.cuda.is_available()
-device = torch.device('cuda:0' if cuda else 'cpu')
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+# @profile
+def main(opt):
+    os.makedirs('output', exist_ok=True)
+    os.makedirs('checkpoints', exist_ok=True)
 
-classes = load_classes(opt.class_path)
+    cuda = torch.cuda.is_available()
+    device = torch.device('cuda:0' if cuda else 'cpu')
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-# Get data configuration
-data_config = parse_data_config(opt.data_config_path)
-train_path = data_config['train']
+    torch.manual_seed(1)
+    classes = load_classes(opt.class_path)
 
-# Get hyper parameters
-hyperparams = parse_model_config(opt.model_config_path)[0]
-lr = float(hyperparams['learning_rate'])
-momentum = float(hyperparams['momentum'])
-decay = float(hyperparams['decay'])
-burn_in = int(hyperparams['burn_in'])
+    # Get data configuration
+    # data_config = parse_data_config(opt.data_config_path)
+    train_path = '/Users/glennjocher/Downloads/DATA/xview/'  # data_config['train']
+    if cuda:
+        train_path = '../'
 
-# Initiate model
-model = Darknet(opt.model_config_path)
-# model.load_weights(opt.weights_path)
-model.apply(weights_init_normal)
-model.to(device)
-model.train()
+    # Get hyper parameters
+    hyperparams = parse_model_config(opt.model_config_path)[0]
+    lr = float(hyperparams['learning_rate'])
+    momentum = float(hyperparams['momentum'])
+    decay = float(hyperparams['decay'])
+    burn_in = int(hyperparams['burn_in'])
 
-# Get dataloader
-dataloader = DataLoader(ListDataset(train_path),
-    batch_size=opt.batch_size, shuffle=False, num_workers=opt.n_cpu)
+    # Initiate model
+    model = Darknet(opt.model_config_path)
+    # model.load_weights(opt.weights_path)
+    model.apply(weights_init_normal)
+    model.to(device)
+    model.train()
 
-optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, dampening=0, weight_decay=decay)
+    # Get dataloader
+    dataloader = DataLoader(ListDataset_xview(train_path, opt.img_size),
+                            batch_size=opt.batch_size, shuffle=False, num_workers=opt.n_cpu)
 
-for epoch in range(opt.epochs):
-    for batch_i, (_, imgs, targets) in enumerate(dataloader):
-        imgs = Variable(imgs.type(Tensor))
-        targets = Variable(targets.type(Tensor), requires_grad=False)
+    #optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, dampening=0, weight_decay=decay)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0002)
 
-        optimizer.zero_grad()
+    for epoch in range(opt.epochs):
+        t0 = time.time()
+        epochAP, nGT = 0, 0
+        print('\n' + '%10s' * 12 % ('Epoch', 'Batch', 'x', 'y', 'w', 'h', 'conf', 'cls', 'total', 'AP', 'mAP', 'time'))
+        for batch_i, (_, imgs, targets, nT) in enumerate(dataloader):
+            imgs = imgs.type(Tensor)
+            targets = targets.type(Tensor)
 
-        loss = model(imgs, targets)
+            loss = model(imgs, targets)
+            #detections = non_max_suppression(model(imgs[3].unsqueeze(0)), opt.conf_thres, opt.nms_thres)
 
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        print('[Epoch %d/%d, Batch %d/%d] [Losses: x %f, y %f, w %f, h %f, conf %f, cls %f, total %f, AP: %.5f]' %
-              (epoch, opt.epochs, batch_i, len(dataloader),
-               model.losses['x'], model.losses['y'], model.losses['w'],
-               model.losses['h'], model.losses['conf'], model.losses['cls'],
-               loss.item(), model.losses['AP']))
+            # s = '[Epoch %d/%d, Batch %d/%d] [x %f, y %f, w %f, h %f, conf %f, cls %f, total %f, AP: %.5f] %.3fs' % (
+            #     epoch, opt.epochs, batch_i, len(dataloader),
+            #     model.losses['x'], model.losses['y'], model.losses['w'],
+            #     model.losses['h'], model.losses['conf'], model.losses['cls'],
+            #     loss.item(), model.losses['AP'], time.time() - t0)
 
-        model.seen += imgs.shape[0]
+            epochAP = (epochAP*batch_i + model.losses['AP'])/(batch_i + 1)
+            nGT += model.losses['nGT']
+            s = ('%10s%10s' + '%10.3g' * 10) % (
+                '%g/%g' % (epoch, opt.epochs), '%g/%g' % (batch_i, len(dataloader)),
+                model.losses['x'], model.losses['y'], model.losses['w'],
+                model.losses['h'], model.losses['conf'], model.losses['cls'],
+                loss.item(), model.losses['AP'], epochAP, time.time() - t0)
+            print(s)
+            with open('printedResults.txt', 'a') as file:
+                file.write(s + '\n')
 
-    if epoch % opt.checkpoint_interval == 0:
-        model.save_weights('%s/%d.weights' % (opt.checkpoint_dir, epoch))
+            model.seen += imgs.shape[0]
+            t0 = time.time()
+
+        if epoch % opt.checkpoint_interval == 0:
+            torch.save(model.state_dict(), '%s/epoch%d.pt' % (opt.checkpoint_dir, epoch))
 
 
+if __name__ == '__main__':
+    main(opt)
+    os.system('sudo shutdown')
--- utils/gcp.sh
+++ utils/gcp.sh
@@ -6,17 +6,17 @@
 #
 ##clone repo
 #sudo rm -rf yolov3
-#git clone https://github.com/ultralytics/yolov3
+git clone https://github.com/ultralytics/yolov3
 #
 ## move training data to folder
-#sudo mv train_images yolov3
-#sudo mv train_labels yolov3
+sudo mv train_images yolov3
+sudo mv train_labels yolov3
 
 # do training
-python3  train_xview.py -img_size 864 -batch_size 4 -epochs 30
+python3  train_xview.py -img_size 864 -batch_size 4 -epochs 10
 
 # move training data out
 #sudo mv train_images ..
 #sudo mv train_labels ..
 
-sudo shutdown
+#sudo shutdown
