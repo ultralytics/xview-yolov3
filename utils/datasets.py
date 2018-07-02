56cb08bbb5ebc44d405ddb443e3b05d3de58adb0
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-02 15:09:38 +0200

    updates

--- utils/datasets.py
+++ utils/datasets.py
@@ -242,7 +242,7 @@ class ListDataset_xview_crop():  # for training
             h, w, _ = img0.shape
             # scipy.io.savemat('testImage.mat',{'img': img0.astype(np.float32)})
             # %timeit scipy.io.loadmat('testImage.mat')
-            for j in range(8):
+            for j in range(16):
                 padx = int(random.random() * (w - self.height))
                 pady = int(random.random() * (h - self.height))
                 img = img0[pady:pady + self.height, padx:padx + self.height]
