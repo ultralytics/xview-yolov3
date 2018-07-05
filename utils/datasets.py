c6ccf8e2dcee608b808f17cfcabc5ecf5bd8df45
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-05 18:37:38 +0200

    updates

--- utils/datasets.py
+++ utils/datasets.py
@@ -130,15 +130,16 @@ class ListDataset_xview_crop():  # for training
                     labels = np.array([], dtype=np.float32)
 
                 # plot
-                import matplotlib.pyplot as plt
-                plt.subplot(4, 4, j + 1).imshow(img[:, :, ::-1])
-                plt.plot(labels[:, [1, 3, 3, 1, 1]].T, labels[:, [2, 2, 4, 4, 2]].T, '.-')
+                # import matplotlib.pyplot as plt
+                # plt.subplot(4, 4, j + 1).imshow(img[:, :, ::-1])
+                # plt.plot(labels[:, [1, 3, 3, 1, 1]].T, labels[:, [2, 2, 4, 4, 2]].T, '.-')
 
                 # random affine
-                if random.random() > 0.8:
-                    img, labels = random_affine(img, targets=labels, degrees=(-15, 15), translate=(0.05, 0.05),
+                if random.random() > 0.9:
+                    img, labels = random_affine(img, targets=labels, degrees=(-10, 10), translate=(0.05, 0.05),
                                                 scale=(.9, 1.1))
-                # plt.subplot(2, 2, 2).imshow(img[:, :, ::-1])
+
+                # plt.subplot(4, 4, j+1).imshow(img[:, :, ::-1])
                 # plt.plot(labels[:, [1, 3, 3, 1, 1]].T, labels[:, [2, 2, 4, 4, 2]].T, '.-')
 
                 nL = len(labels)
