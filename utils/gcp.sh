948afdced2b8d71047ab7ceb3b940f7934663ff1
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-08 01:45:56 +0200

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -18,7 +18,7 @@ mkdir yolo/checkpoints
 cd yolo
 python3 train.py -epochs 9999
 
-python3 detect.py -config_path 'cfg/yolovx_YL0.cfg' -weights_path 'checkpoints/fresh3.pt'
+python3 detect.py -config_path 'cfg/yolovx_YL0.cfg' -weights_path 'checkpoints/fresh3.pt' -conf_thres 0.99
 
 # move checkpoint to fuse drive
 # google-drive-ocamlfuse drive
