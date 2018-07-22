dcd53aa979733fba5370fd1cc4fe016ebd549040
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-22 20:31:14 +0200

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -30,13 +30,16 @@ python3 detect.py -config_path 'cfg/yolovx_YL0.cfg' -weights_path 'checkpoints/f
 # get xview training data
 wget -O train_images.tgz 'https://d307kc0mrhucc3.cloudfront.net/train_images.tgz?Expires=1531850600&Signature=FvYp1qDdadcfOAF2ELmHSJXaRYkq~R2KrRi0Fk3akL1~UZWiCm26QjLh52e11Ga-99GNAkLylXauCgt0k17hmw2aMrMDW-z9Qo9hGQV-BkYEimhd~dyybOqqGJ3ZWG3CmeesHHJ7ScdDpv9aIxZTNo-QUSABA8g5X2oMs96RWOy-GnAw09W8liBIoLAfeoGcqOubvY7vOMtFeFgFatzmMSoLPQ-Y8Zv2bGpQyih-pd7A2S0VAE3ccDwvjKgdOgYeuZLXBNaF5Wy~-JNX2RdaqaXmLO42P3soxT5FnCnGbLYoVAI7K6-mtlttcw0VOTMXqWvoN8QOsdZenREhKfJ0iw__&Key-Pair-Id=APKAIKGDJB5C3XUL2DXQ'
 tar -xvzf train_images.tgz
-sudo rm -rf train_images/._*
+sudo rm -rf train_images/._* train_images/659.tif train_images/769.tif
 
 # convert all .tif to .bmp
-sudo rm -rf yolo && git clone https://github.com/ultralytics/yolo && cd yolo && python3
+sudo rm -rf yolo && git clone https://github.com/ultralytics/yolo && cd yolo
+
+python3
 from utils import datasets
 datasets.convert_tif2bmp_clahe('../train_images')
 exit()
+
 
 rm -rf 149.bmp
 rm -rf 374.bmp
