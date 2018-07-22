5f1203b56f201321a532a649eeeae934ba44e62c
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-22 21:37:00 +0200

    updates

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ def main(opt):
     dataloader = ListDataset(train_path, batch_size=opt.batch_size, img_size=opt.img_size, targets_path=targets_path)
 
     # reload saved optimizer state
-    resume_training = True
+    resume_training = False
     if resume_training:
         state = model.state_dict()
         pretrained_dict = torch.load('checkpoints/c0.pt', map_location='cuda:0' if cuda else 'cpu')
