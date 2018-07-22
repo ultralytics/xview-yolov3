4a047ce449fc230f2173c1d5f287dbfab9eb1d49
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-22 23:21:55 +0200

    updates

--- models.py
+++ models.py
@@ -169,7 +169,6 @@ class YOLOLayer(nn.Module):
             nM = mask.sum().float()
             nGT = sum([len(x) for x in targets])
             if nM > 0:
-                # print(tx[mask].mean().item(),ty[mask].mean().item(),tw[mask].mean().item(),th[mask].mean().item())
                 wC = self.class_weights[torch.argmax(tcls, 1)]  # weight class
                 wC /= sum(wC)
                 lx = MSELoss(x[mask], tx[mask])
@@ -181,12 +180,11 @@ class YOLOLayer(nn.Module):
 
                 # lcls = nM * (BCEWithLogitsLoss1_reduceFalse(pred_cls[mask], tcls.float()) * wC.unsqueeze(1)).sum() / self.nC
                 # lcls = (BCEWithLogitsLoss1_reduceFalse(pred_cls[mask], tcls.float()) * self.weight.unsqueeze(0)).sum() / self.nC
-
                 lcls = CrossEntropyLoss(pred_cls[mask], torch.argmax(tcls, 1))
             else:
                 lx, ly, lw, lh, lcls, lconf = FT([0]), FT([0]), FT([0]), FT([0]), FT([0]), FT([0])
 
-            lconf += (1.5 * nGT) * BCEWithLogitsLoss0(pred_conf[~mask], mask[~mask].float())
+            lconf += (1 * nGT) * BCEWithLogitsLoss0(pred_conf[~mask], mask[~mask].float()) ** 2
             loss = lx + ly + lw + lh + lconf + lcls
 
             i = F.sigmoid(pred_conf[~mask]) > 0.999
