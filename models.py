b64109ec93c497adf8afb629dfd975f14a8f836a
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-25 15:52:53 +0200

    updates

--- models.py
+++ models.py
@@ -174,8 +174,8 @@ class YOLOLayer(nn.Module):
                 ly = MSELoss(y[mask], ty[mask])
                 lw = MSELoss(w[mask], tw[mask])
                 lh = MSELoss(h[mask], th[mask])
-                lconf = 1.25 * BCEWithLogitsLoss1(pred_conf[mask], mask[mask].float())
-                # lconf = 1.25 * nM * (BCEWithLogitsLoss1_reduceFalse(pred_conf[mask], mask[mask].float()) * wC).sum()
+                # lconf = 1.25 * BCEWithLogitsLoss1(pred_conf[mask], mask[mask].float())
+                lconf = 1.25 * nM * (BCEWithLogitsLoss1_reduceFalse(pred_conf[mask], mask[mask].float()) * wC).sum()
 
                 lcls = CrossEntropyLoss(pred_cls[mask], torch.argmax(tcls, 1)) * nM * 0.125
                 # lcls = (BCEWithLogitsLoss1_reduceFalse(pred_cls[mask], tcls.float()) * wC.unsqueeze(1)).sum()
