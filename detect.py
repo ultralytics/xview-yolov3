7e4fb0c60d5c67c2966ad3b7c3dd91e9cfa3165f
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-08-05 22:50:09 +0200

    updates

--- detect.py
+++ detect.py
@@ -32,7 +32,6 @@ parser.add_argument('-img_size', type=int, default=32 * 51, help='size of each i
 opt = parser.parse_args()
 print(opt)
 
-
 def detect(opt):
     if opt.plot_flag:
         os.system('rm -rf ' + opt.output_folder + '_img')
