a434259efd9aea9aa94e9642704820041f1ef95a
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-08-10 15:30:27 +0200

    updates

--- models.py
+++ models.py
@@ -139,7 +139,8 @@ class YOLOLayer(nn.Module):
             BCEWithLogitsLoss0 = nn.BCEWithLogitsLoss()
             BCEWithLogitsLoss2 = nn.BCEWithLogitsLoss(weight=weight, reduction='sum')
             MSELoss = nn.MSELoss(reduction='sum')
-            # CrossEntropyLoss = nn.CrossEntropyLoss(weight=weight)
+            CrossEntropyLoss = nn.CrossEntropyLoss(weight=weight, reduction='sum')
+
 
             if requestPrecision:
                 gx = self.grid_x[:, :, :nG, :nG]
@@ -168,8 +169,8 @@ class YOLOLayer(nn.Module):
                 lh = 1 * MSELoss(h[mask], th[mask])
                 lconf = 1.25 * BCEWithLogitsLoss1(pred_conf[mask], mask[mask].float())
 
-                # lcls = .125 * nM * CrossEntropyLoss(pred_cls[mask], torch.argmax(tcls, 1))
-                lcls = 1.25 * 15 * BCEWithLogitsLoss2(pred_cls[mask], tcls.float())
+                lcls = 1.25 * CrossEntropyLoss(pred_cls[mask], torch.argmax(tcls, 1).long())
+                lcls += 1.25 * BCEWithLogitsLoss2(pred_cls[mask], tcls.float())
             else:
                 lx, ly, lw, lh, lcls, lconf = FT([0]), FT([0]), FT([0]), FT([0]), FT([0]), FT([0])
 
